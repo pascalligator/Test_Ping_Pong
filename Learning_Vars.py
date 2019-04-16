05ca41f6cca01d4f3ebfc75f8e01365367b27fa3
Pascal Popp <popp93@gmail.com>
2019-04-17 00:29:56 +0200

    margin befor alternation inserted

--- Learning_Vars.py
+++ Learning_Vars.py
@@ -2,4 +2,9 @@ population_size = 50
 true_copies = int(population_size * 0.2)
 mutated_copies = int(population_size * 0.4)
 added_randoms = population_size - true_copies - mutated_copies
+# abweichung durch anspielzufall ungefähr +/- 2% der normalen fitness (anspiele auf gegner)
+# daher ungefähr im bereich 4%
+# mal zwei zur sicherheit
+# daher ahead = 0.02 * 2 * 2 * population_size**2
+ahead = int(0.02 * 4 * population_size**2)
 generations = 3
--- Main.py
+++ Main.py
@@ -8,17 +8,19 @@ class Main():
         for i in range(lng.lng_vars.generations):
             print(i)
             dif = 0
-            while dif <= 0:
-                self.spezies.evaluate()
-                dif = sum([x.fitness for x in self.spezies.population_one]) - sum([x.fitness for x in self.spezies.population_two])
-                self.spezies.new_generation()
+            while dif <= lng.lng_vars.ahead:
                 print("left player sucks")
-            self.spezies.trainee = 1
-            while dif >= 0:
                 self.spezies.evaluate()
                 dif = sum([x.fitness for x in self.spezies.population_one]) - sum([x.fitness for x in self.spezies.population_two])
+                print(dif)
                 self.spezies.new_generation()
+            self.spezies.trainee = 1
+            while dif >= -lng.lng_vars.ahead:
                 print("right player sucks")
+                self.spezies.evaluate()
+                dif = sum([x.fitness for x in self.spezies.population_one]) - sum([x.fitness for x in self.spezies.population_two])
+                print(dif)
+                self.spezies.new_generation()
             self.spezies.tranee = 0
     def save(self):
         pkl.dump(self.spezies, open( "save.p", "wb" ))
